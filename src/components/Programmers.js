684d079ce16709a2fe37efe4793ab47d8509a006
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-15 18:04:41 -0500

    Refractor getNameOfFeatured to 1 line ^^

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -36,11 +36,12 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    let featName;
-    progs.forEach((prog) =>
-      prog.id === featProg ? (featName = prog.name) : null
-    );
-    return featName;
+    // let featName;
+    // progs.forEach((prog) =>
+    //   prog.id === featProg ? (featName = prog.name) : null
+    // );
+    // return featName;
+    return progs.find((prog) => prog.id === featProg).name;
   };
 
   const style = {
