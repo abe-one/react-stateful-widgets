15e1ee1038a5a457ab5ec2205c4bb12ac1be6e47
abe-one <77995299+abe-one@users.noreply.github.com>
2021-03-15 17:17:17 -0500

    Squares.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -37,17 +37,16 @@ STEP 6:
 import React, { useState } from "react";
 
 export default function Input() {
-  const [inputValue, setInputValue] = useState("ee");
+  const [inputValue, setInputValue] = useState("");
 
   const changeInput = (evt) => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
-    console.log(value);
     setInputValue(value);
   };
   const reset = () => {
-    /* STEP 5 */
+    setInputValue("");
   };
 
   const style = {
@@ -59,9 +58,17 @@ export default function Input() {
   return (
     <div className="widget-input container">
       <h2>Input</h2>
-      <div id="output" style={style}></div> {inputValue.toUpperCase()}
+      <div id="output" style={style}>
+        {inputValue.toUpperCase()}
+      </div>
       <div>
-        <input id="input" type="text" onChange={changeInput} /> {/* STEP 6 */}
+        <input
+          id="input"
+          type="text"
+          value={inputValue}
+          onChange={changeInput}
+        />{" "}
+        {/* STEP 6 */}
         <button id="resetInput" onClick={reset}>
           Reset
         </button>
